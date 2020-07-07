4e9570e580e0d1821eb63c8ff01ceae7736e4b4c
Osmond Ong <ong.osmond@yahoo.com>
2020-07-07 19:02:47 +0800

    Used template literals line breaks spacing to make the readme text more readable in-code.

--- index.js
+++ index.js
@@ -48,7 +48,7 @@ inquirer
         const licenceBadge = (response.license == "ISC") ?
             "[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)]" : "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)]";
         const licenceNotice = (response.license == "ISC") ?
-            `Copyright (c) 2020, https://github.com/${response.gitHubUserName}
+            `ISC License. Copyright (c) 2020, https://github.com/${response.gitHubUserName}
             Permission to use, copy, modify, and/or distribute this software for any purpose with or without fee is hereby granted, provided that the above copyright notice and this permission notice appear in all copies.
             THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.` :
             `The MIT License (MIT)
@@ -69,7 +69,19 @@ inquirer
             OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
             THE SOFTWARE.`;
         fs.writeFile(`${response.title}.md`,
-            `## Project Title: ${response.title} ${licenceBadge} \n\n## Description: \n${response.description} \n\n## Table of Contents \n* [Usage Information](#Usage) \n* [Contribution](#Contribution) \n* [Test Instructions](#Test) \n* [Questions](#Questions) \n* [License](#License)  \n\n## Usage: \n${response.usageInformation} \n\n## Contribution: \n${response.contribution} \n\n## Test: \n${response.tests} \n\n## Questions: \n Please visit my [GitHub Page](https://github.com/${response.gitHubUserName}/) or email me your questions at ${response.emailAddress}. I am available 8am-4pm Monday-Friday (Australian Western Standard Time). \n\n## License: \n${licenceNotice} `,
+            (`## Project Title: ${response.title} ${licenceBadge} \n` +
+            `\n## Description: \n${response.description} \n` +
+            `\n## Table of Contents \n` + 
+            `* [Usage Information](#Usage) \n` +
+            `* [Contribution](#Contribution) \n` +
+            `* [Test Instructions](#Test) \n` +
+            `* [Questions](#Questions) \n` +
+            `* [License](#License)  \n` +
+            `\n## Usage: \n${response.usageInformation} \n` +
+            `\n## Contribution: \n${response.contribution} \n` +
+            `\n## Test: \n${response.tests} \n` +
+            `\n## Questions: \n Please visit my [GitHub Page](https://github.com/${response.gitHubUserName}/) or email me your questions at ${response.emailAddress}. I am available 8am-4pm Monday-Friday (Australian Western Standard Time). \n` +
+            `\n## License: \n${licenceNotice} `),
             function(err) {
                 if (err) {
                     return console.log(err);
